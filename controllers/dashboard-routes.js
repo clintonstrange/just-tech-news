b7551a0ac138fceed18c44a902949e495e347f93
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2021-01-15 17:16:15 -0600

    bugs

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
+const { Post, User, Comment, Vote } = require("../models");
 const withAuth = require("../utils/auth");
 
+// get all posts for dashboard
 router.get("/", withAuth, (req, res) => {
+  console.log(req.session);
+  console.log("======================");
   Post.findAll({
     where: {
-      // use the ID from the session
       user_id: req.session.user_id,
     },
     attributes: [
@@ -37,7 +39,6 @@ router.get("/", withAuth, (req, res) => {
     ],
   })
     .then((dbPostData) => {
-      // serialize data before passing to template
       const posts = dbPostData.map((post) => post.get({ plain: true }));
       res.render("dashboard", { posts, loggedIn: true });
     })
@@ -92,7 +93,5 @@ router.get("/edit/:id", withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
-
-module.exports = router;
 
 module.exports = router;
